b25ab8a20ca672663e7a8ebf56d6261c6cdbe981
Kristijan Isajloski <kikoisajloski@gmail.com>
2020-04-12 20:25:33 +0200

    changes

---
+++ canvas.java
@@ -0,0 +1,12 @@
+class Canvas{
+	private List<Points> lista;
+
+	public void delete(){
+ while (itr.hasNext()) 
+        { 
+            int x = (Integer)itr.next(); 
+            if (x < 10) 
+                itr.remove(); 
+        } 
+}
+}
